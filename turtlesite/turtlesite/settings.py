547465489fb54dfcbf0a91593a3e6f04be140f54
b <b@nbxy.us>
2013-05-20 17:10:18 +0800

    ...
    Signed-off-by: b <b@nbxy.us>

--- turtlesite/turtlesite/settings.py
+++ turtlesite/turtlesite/settings.py
@@ -13,7 +13,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         #'NAME': 'turtle',                      # Or path to database file if using sqlite3.
-        'NAME': '/home/0common/d/tw/turtlesite/db/sqlite.file',                      # Or path to database file if using sqlite3.
+        'NAME': '/opt/db/sqlite.file',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'turtleweb',
         'PASSWORD': 'turtleweb123',
