322788811728ad08498ef0b56f7731ad69c6ac45
b <b@nbxy.us>
2013-05-20 15:53:11 +0800

    ...
    Signed-off-by: b <b@nbxy.us>

--- turtlesite/turtlesite/settings.py
+++ turtlesite/turtlesite/settings.py
@@ -4,15 +4,16 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+     ('Bin Jiang', 'b@nbxy.us'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'turtle',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        #'NAME': 'turtle',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/0common/d/tw/turtlesite/db/sqlite.file',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'turtleweb',
         'PASSWORD': 'turtleweb123',
